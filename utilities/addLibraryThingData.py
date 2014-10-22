e5fb89d3a5ca8b1f9a38f6b2bfe24aeb32af0246
Mouse Reeve <mouse@radius.com>
2014-10-21 20:01:37 -0700

    Adds librarything scraper json

---
+++ utilities/addLibraryThingData.py
@@ -0,0 +1,42 @@
+import json
+import urllib2
+import neo4jrestclient.client as client
+from neo4jrestclient.client import GraphDatabase
+from dbTools import *
+
+gdb = GraphDatabase("http://localhost:7474/db/data/")
+
+url = 'https://raw.githubusercontent.com/mouse-reeve/book-scraper/master/items.json'
+response = urllib2.urlopen(url)
+data = json.load(response)
+
+for bookData in data:
+    if 'isbn' in bookData:
+        isbn = bookData['isbn']
+        book = findByISBN(isbn)
+        print bookData['isbn']
+
+        if 'characters' in bookData:
+            params = {'type': 'character', 'anonymize': 0}
+            for character in bookData['characters']:
+                try:
+                    buildRelationship(book, character, 'person', 'has the character', params)
+                except:
+                    pass
+        if 'places' in bookData:
+            for place in bookData['places']:
+                try:
+                    buildRelationship(book, place, 'location', 'is set in')
+                except:
+                    pass
+
+        if 'year' in bookData:
+            try:
+                year = int(bookData['year'])
+                decade = (year/10)*10
+                buildRelationship(book, str(year), 'year', 'first published in', {'year': year})
+                buildRelationship(book, str(decade), 'year', 'from the decade', {'year': decade})
+            except:
+                pass
+
+
