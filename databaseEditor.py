bba6c363afc35488f00f58fb742017dc638711a1
Mouse Reeve <mouse@radius.com>
2014-10-27 14:37:41 -0700

    Adds some more functionality
    A little spotty. Sorry.

--- databaseEditor.py
+++ databaseEditor.py
@@ -1,9 +1,16 @@
 from neo4jrestclient.client import GraphDatabase, Node
+import csv
+import urllib2
 
 class DatabaseEditor:
     ''' updates and modifies the book database '''
 
+
     def __init__(self):
+        githubBaseUrl = 'https://raw.githubusercontent.com/mouse-reeve'
+        self.canonicalCSV = '/book-catalogue/master/canonical.csv'
+        self.libraryThingCSV = '/book-catalogue/master/libraryThing.csv'
+
         self.gdb = GraphDatabase("http://localhost:7474/db/data/")
 
         # get list of graphs (each graph has a different label)
@@ -18,6 +25,7 @@ class DatabaseEditor:
                 continue
             self.graphNames.append(graph)
 
+
     def getNodeById(self, nodeId):
         q = "MATCH n WHERE id(n)=%d RETURN n" % nodeId
         nodes = self.gdb.query(q, returns=Node)
@@ -26,21 +34,32 @@ class DatabaseEditor:
 
         return nodes[0][0]
 
+
     def isValidGraphName(self, graphName):
         return graphName in self.graphNames
+
 
     def getAvailableNodes(self, graphName):
         if not self.isValidGraphName(graphName):
             print 'That\'s not a real graph. You can\'t fool me'
             return False
-        q = 'MATCH (n:%s) WHERE n.weight>0 AND n.available RETURN n ORDER BY n.weight DESC' % graphName
+
+        q = 'MATCH (n:%s) WHERE n.weight>0' % graphName
+        q += 'AND n.available RETURN n ORDER BY n.weight DESC'
         nodes = self.gdb.query(q, returns=Node)
         return nodes
 
-    def addBooks(self, graph):
-        # download canonical book csv
 
-        # find and add new books - assumes list is ordered by date added
+    def addBooks(self, graphName):
+        if not self.isValidGraphName(graphName):
+            return False
+
+        response = urllib2.urlopen(self.canonicalCSV)
+        reader = csv.DictReader(response)
+
+        for book in reversed(reader):
+            print book
+            # add book to graph
 
         # download libraryThing book csv
 
@@ -49,14 +68,43 @@ class DatabaseEditor:
         # build relationships for new books
         pass
 
-    def removeField(self, fieldName, value):
+
+    def removeField(self, graphName, fieldName, value):
+        if not self.isValidGraphName(graphName):
+            return False
+
         # Find all nodes with that field:
+        q = 'MATCH (n:%s) WHERE "%s" IN n.%s RETURN n' % (graphName, value, fieldName)
+        nodes = self.gdb.query(q, returns=Node)
 
-        #   Remove field from node properties
+        for node in nodes:
+            node = node[0]
+            # Remove field from node properties
 
-        #   Find relationships from node that include field:
-        #       Decrement relationship weight
-        #       Remove field from relationship field list
-        pass
+            edges = node.relationships
+            for edge in edges:
+                # Reject nodes that do not contain field
+                # Decrement relationship weight
+                # Remove field from relationship field list
+                continue
+
+
+    def findByISBN(self, graphName, isbn):
+        if not self.isValidGraphName(graphName):
+            return False
+
+        q = 'MATCH (n:%s) WHERE n.isbn = "%s" RETURN n' % (graphName, isbn)
+        nodes = self.gdb.query(q, returns=Node)
+
+        if len(nodes) > 0 and len(nodes[0]) > 0:
+            return nodes[0][0]
+        else:
+            # checks for alternate ISBN format used by LibraryThing
+            variant = isbn[0:-1]
+            q = 'MATCH (n:%s) WHERE n.isbn =~ ".*%s.*" RETURN n' % (graphName, variant)
+            nodes = self.gdb.query(q, returns=Node)
+            if len(nodes) > 0 and len(nodes[0]) > 0:
+                return nodes[0][0]
+        return False
 
 
