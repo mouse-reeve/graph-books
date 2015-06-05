eb682745319d7b9a1579edac6bd95b3957d417fc
Mouse Reeve <mouse.reeve@gmail.com>
2015-06-05 13:44:34 -0700

    Updated version of neo4j. This script is dumb tho

--- deleteDatabase.sh
+++ deleteDatabase.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/Applications/neo4j-community-2.1.5/bin/neo4j stop
-rm -rf /Applications/neo4j-community-2.1.5/data/*
-/Applications/neo4j-community-2.1.5/bin/neo4j start
+/Applications/neo4j-community-2.2.2/bin/neo4j stop
+rm -rf /Applications/neo4j-community-2.2.2/data/*
+/Applications/neo4j-community-2.2.2/bin/neo4j start
